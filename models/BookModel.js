987dbdd6bc2596e361fb566134285214d3e7aa6a
rawanazazi12 <rawanazazi12@gmail.com>
2021-08-14 16:15:34 +0300

    changes

--- models/BookModel.js
+++ models/BookModel.js
@@ -1,6 +1,6 @@
 class Book{
     constructor(data){
-        this.image=data.volumeInfo["imageLinks"];
+        this.image=data.volumeInfo.imageLinks;
         this.description=data.volumeInfo.description;
         this.title=data.volumeInfo.title;
         
