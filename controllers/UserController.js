99e909d9b2a6725550bb6c7bac7efcbac9b55a9e
leen-duafi <leen-hala@hotmail.com>
2021-08-18 10:59:50 +0300

    ok

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,34 +1,35 @@
-const {bookModel}=require('../models/UserModel');
+const { bookModel } = require('../models/UserModel');
 
-const getUser=(req,res)=>{
-    const {email}=req.query;
+const getUser = (req, res) => {
+    const { email } = req.query;
     console.log(email);
-    bookModel.find({email:email},(error,user)=>{
-        try{
+    bookModel.find({ email: email }, (error, user) => {
+        try {
             res.send(user);
         }
-        catch(error){
+        catch (error) {
             res.send(error);
         }
     });
 }
 
 
-const createBook=(req,res)=>{
-    const email=req.body.email;
-    const book=req.body.book;
-    bookModel.findOne({email:email},(error,userInfo)=>{
-        if (userInfo===null){
+const createBook = (req, res) => {
+    const email = req.body.email;
+    const book = req.body.book;
+    console.log(req.body);
+    bookModel.findOne({ email: email }, (error, userInfo) => {
+        if (userInfo === null) {
             res.send(error);
         }
-        else{
+        else {
             userInfo.booksAdded.push(book);
             userInfo.save();
             res.send(userInfo);
         }
-        
+
     });
 }
 
 
-module.exports={createBook,getUser};
+module.exports = { createBook, getUser };
--- server.js
+++ server.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
-const { getBook, fiction, nonFiction, kids, classic, getQuote } = require('./controllers/BookController');
-const {createBook,getUser}=require('./controllers/UserController');
-const {seedBookCollection}=require('./models/UserModel');
+const { getBook, fiction, nonFiction, kids, classic, getQuote, creatReview } = require('./controllers/BookController');
+const { createBook, getUser } = require('./controllers/UserController');
+const { seedBookCollection } = require('./models/UserModel');
 // seedBookCollection();
 app.get('/',
     function (req, res) {
@@ -19,13 +20,14 @@ mongoose.connect(`${process.env.MONGO_URL}/bookShop`, { useNewUrlParser: true, u
 mongoose.set('useCreateIndex', true);
 
 app.get('/book', getBook);////search bar 
-app.get('/user',getUser);
-app.post('/book',createBook)
+app.get('/user', getUser);
+app.post('/book', createBook)
 app.get('/book1', fiction);
 app.get('/book2', nonFiction);
 app.get('/book3', classic);
 app.get('/book4', kids);
 app.get('/quote', getQuote);
+app.post('/review', creatReview);//////creat review
 app.listen(PORT, () => {
     console.log(`Server Started on ${PORT}`)
 });
